bcae8a06ecfbc75e336ca2bdd6c3df5a221f68fe
Perfspec <lewisrwebb@gmail.com>
2018-10-26 11:04:10 +0100

    init Commit

--- api-sample/src/main/java/com/qa/persistence/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.qa.persistence.repository;
-
-public interface AccountRepository {
-
-	String getAllAccounts();
-	String createAccount(String accout);
-	String deleteAccount(Long id);
-
-}
